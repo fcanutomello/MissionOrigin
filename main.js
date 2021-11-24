310030713c536c9e1e1dea935f0bf39482e3e56d
Felipe Canuto <fcanuto@Felipes-MacBook-Air.local>
2021-11-23 23:23:44 -0300

    Aula 03 - Novos Controles part 1

--- main.js
+++ main.js
@@ -25,3 +25,14 @@ window.addEventListener('scroll', function(){
         header.classList.remove('scroll')
     }
 })
+
+/*Testimonials slider */
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el:'.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+    
+})
