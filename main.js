e133373afb4cc3b734c3cf504f4b9944d5795db3
Felipe Canuto <fcanuto@Felipes-MacBook-Air.local>
2021-11-29 22:35:27 -0300

    Aula 03 - Novos Controles part 2

--- main.js
+++ main.js
@@ -35,4 +35,23 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true,
     
-})
+});
+
+/*Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+  })
+  
+  scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links
+    `,
+    { interval: 100 }
+  )
+
